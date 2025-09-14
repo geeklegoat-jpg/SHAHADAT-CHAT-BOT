53d2af8bbcaf212a84fc65a54024abc84872d252
geeklegoat-jpg <geeklegoat@gmail.com>
2025-09-14 09:59:57 +0200

    Update Help.js

--- Script/commands/Help.js
+++ Script/commands/Help.js
@@ -6,7 +6,7 @@ module.exports.config = {
     name: "help",
     version: "2.0.0",
     hasPermssion: 0,
-    credits: "SHAHADAT SAHU",
+    credits: "ᎬᎷᏢᎬᏒᎬᏌᏒ ᏒᎾᎷᎬᎾ",
     description: "Shows all commands with details",
     commandCategory: "system",
     usages: "[command name/page number]",
@@ -32,7 +32,7 @@ module.exports.languages = {
 ┣━━━━━━━━━━━━━━━━┫
 ┃ ⚙ Prefix: %8
 ┃ 🤖 Bot Name: %9
-┃ 👑 Owner: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔
+┃ 👑 Owner: 🎧এꔰꜛROMEOꜛꗄꔰ🖤󰦉
 ╰━━━━━━━━━━━━━━━━╯`,
         "helpList": "[ There are %1 commands. Use: \"%2help commandName\" to view more. ]",
         "user": "User",
@@ -85,7 +85,7 @@ module.exports.handleEvent = function ({ api, event, getText }) {
         command.config.commandCategory || "Unknown",  
         command.config.cooldowns || 0,  
         prefix,  
-        global.config.BOTNAME || "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭"  
+        global.config.BOTNAME || "🎧এꔰꜛ𝐑𝐎𝐌𝐄𝐎𝐁𝐎𝐓ꜛꗄꔰ🖤󰦉"  
     );  
 
     downloadImages(files => {  
@@ -115,7 +115,7 @@ module.exports.run = function ({ api, event, args, getText }) {
             command.config.commandCategory || "Unknown",  
             command.config.cooldowns || 0,  
             prefix,  
-            global.config.BOTNAME || "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭"  
+            global.config.BOTNAME || "🎧এꔰꜛ𝐑𝐎𝐌𝐄𝐎𝐁𝐎𝐓ꜛꗄꔰ🖤󰦉 " 
         );  
 
         downloadImages(files => {  
@@ -148,8 +148,8 @@ module.exports.run = function ({ api, event, args, getText }) {
 ${msg}
 ┣━━━━━━━━━━━━━━━━┫
 ┃ ⚙ Prefix: ${prefix}
-┃ 🤖 Bot Name: ${global.config.BOTNAME || "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭"}
-┃ 👑 Owner: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔
+┃ 🤖 Bot Name: ${global.config.BOTNAME || " 🎧এꔰꜛ𝐑𝐎𝐌𝐄𝐎𝐁𝐎𝐓ꜛꗄꔰ🖤󰦉"}
+┃ 👑 Owner: ᎬᎷᏢᎬᏒᎬᏌᏒ ᏒᎾᎷᎬᎾ
 ╰━━━━━━━━━━━━━━━━╯`;
 
     downloadImages(files => {  
